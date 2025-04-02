35a7fc2a333bb8fd4786736c9ff71994cbfb9a0b
Karolina Nowacka <278427@student.pwr.edu.pl>
2025-04-02 13:49:57 +0200

    add scoring matrix

--- Needleman-Wunsch/solution.r
+++ Needleman-Wunsch/solution.r
@@ -1,1 +1,35 @@
-print("Hello World")
+needleman_wunsch <- function(seq1, seq2, match = 1, mismatch = -1, gap = -1) {
+  len1 <- nchar(seq1)
+  len2 <- nchar(seq2)
+
+  seq1_chars <- c("-", unlist(strsplit(seq1, split = "")))
+  seq2_chars <- c("-", unlist(strsplit(seq2, split = "")))
+
+  scoring_matrix <- matrix(0, nrow = len1 + 1, ncol = len2 + 1)
+  rownames(scoring_matrix) <- seq1_chars
+  colnames(scoring_matrix) <- seq2_chars
+
+  for (i in 1:(len1 + 1)) {
+    scoring_matrix[i, 1] <- (i - 1) * gap
+  }
+  for (j in 1:(len2 + 1)) {
+    scoring_matrix[1, j] <- (j - 1) * gap
+  }
+
+  for (i in 2:(len1 + 1)) {
+    for (j in 2:(len2 + 1)) {
+      char1 <- seq1_chars[i]
+      char2 <- seq2_chars[j]
+      match_score <- ifelse(char1 == char2, match, mismatch)
+      score_diag <- scoring_matrix[i - 1, j - 1] + match_score
+      score_up   <- scoring_matrix[i - 1, j] + gap
+      score_left <- scoring_matrix[i, j - 1] + gap
+
+      scoring_matrix[i, j] <- max(score_diag, score_up, score_left)
+    }
+  }
+
+  return(scoring_matrix)
+}
+
+print(needleman_wunsch("GATTACA", "GCATGCU"))
